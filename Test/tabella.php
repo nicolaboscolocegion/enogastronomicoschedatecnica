0e2f52c2d52707718f8a9eb2832a3dbe8c18ab77
Nicola Boscolo <nicola.boscolocegion.1@studenti.unipd.it>
2023-04-29 10:23:32 +0200

    strumenti aggiunti nel PHP

--- Test/tabella.php
+++ Test/tabella.php
@@ -3,7 +3,7 @@
 
 	<head>
 
-	
+		<meta charset="UTF-8">
 	</head>
 	
 	
@@ -29,7 +29,19 @@
 			<td> <?php echo $_POST["Ingredienti"]; ?></td></tr>
 
 		<tr><td>Strumenti necessari alla preparazione</td>
-			<td> </td></tr>
+			<td> 
+
+			<?php
+			$strumento = $_POST['strumento']; // prendo i dati dal form
+				// Altrimenti itero sugli elementi dell'array, e stampo il valore:
+				for($i = 0; $i < count($strumento); $i++) {
+					echo($strumento[$i] . '<br>');
+				}
+			
+			?>
+
+
+			</td></tr>
 
 		<tr><td>Procedimento del piatto</td>
 			<td><?php echo $_POST["Procedimento"]; ?> </td></tr>
