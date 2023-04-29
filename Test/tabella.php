c11ddc3b1a0b6e1ed105abcf9a0645731b5d0a84
Nicola Boscolo <nicola.boscolocegion.1@studenti.unipd.it>
2023-04-29 10:03:03 +0200

    conversione a PHP

---
+++ Test/tabella.php
@@ -0,0 +1,54 @@
+<html>
+
+
+	<head>
+
+	
+	</head>
+	
+	
+	<body>
+	
+	<table border="1">
+		<tr><td>Nome piatto</td>
+			<td>  <?php echo $_POST["Nome_piatto"]; ?> </td></tr>
+
+		<tr><td>Pax</td>
+			<td> <?php echo $_POST["Pax"]; ?></td></tr>
+
+		<tr><td>Descrizione del piatto</td>
+			<td><?php echo $_POST["descrizione"]; ?> </td></tr>
+
+		<tr><td>Classificazione del piatto</td>
+			<td> <?php echo $_POST["Classificazione"]; ?></td></tr>
+
+		<tr><td>Allergeni</td>
+			<td><?php echo $_POST["Allergeni"]; ?> </td></tr>
+
+		<tr><td>Ingredienti del piatto</td>
+			<td> <?php echo $_POST["Ingredienti"]; ?></td></tr>
+
+		<tr><td>Strumenti necessari alla preparazione</td>
+			<td> </td></tr>
+
+		<tr><td>Procedimento del piatto</td>
+			<td><?php echo $_POST["Procedimento"]; ?> </td></tr>
+
+		<tr><td>Temperatura di conservazione</td>	
+			<td><?php echo $_POST["T_cons"]; ?> </td></tr>
+
+		<tr><td>Data di scadenza</td>
+			<td><?php echo $_POST["d_expired"]; ?> </td></tr>
+
+		<tr><td>Valori nutrizionali del piatto</td>
+			<td> <?php echo $_POST["nutrition"]; ?></td></tr>
+		
+	</table>
+		
+
+
+
+	
+	</body>
+	
+<html> 
